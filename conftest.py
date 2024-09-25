21d530093c5516af8ed2cb3db43b84fdacbfdde4
Timpolll <timpoll87@gmail.com>
2024-09-25 16:27:22 +0500

    Adding files

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+        driver_browser = webdriver.Firefox(options=options)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name and --language should be written")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
