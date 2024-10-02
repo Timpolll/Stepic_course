367f14ae9a6e6d2ad12bcec4dba5db8b4a09aa21
Timpolll <timpoll87@gmail.com>
2024-10-02 17:51:22 +0500

    base_page

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="ru",
                      help="Choose language")
 
 @pytest.fixture(scope="function")
