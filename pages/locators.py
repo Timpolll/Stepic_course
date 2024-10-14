13fce85d3be368255601188e350030f45dddb53e
Timpolll <timpoll87@gmail.com>
2024-10-14 18:01:59 +0500

    4_2_step_8

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,10 @@
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators:
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[value='Log In']")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[value='Register']")
